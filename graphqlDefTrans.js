833724070b07f1381365a8011957cbe8361ff99f
cristichiu <grajdiancristian@gmail.com>
2025-04-18 22:01:02 +0300

    graphql typedefs translation update

---
+++ graphqlDefTrans.js
@@ -0,0 +1,14 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const srcDir = 'src/graphql/defTypes';
+
+let entries = await fs.readdir(srcDir, { recursive: true, withFileTypes: true });
+entries = entries.filter(file => ['.graphql'].some(ext => file.name.endsWith(ext)));
+entries.forEach(async e => {
+    let dst = e.parentPath.replace("src", "dist")
+    let src = e.parentPath
+    await fs.mkdir(dst, { recursive: true })
+    await fs.copyFile(path.join(src, e.name), path.join(dst, e.name))
+    console.log(`copied ✅ ${e.parentPath}/${e.name}`)
+})
--- src/graphql/defTypes/index.ts
+++ src/graphql/defTypes/index.ts
@@ -1,8 +1,15 @@
 import fs from 'fs/promises';
-const files = await fs.readdir("./src/graphql/defTypes", { recursive: true });
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const files = await fs.readdir(__dirname, { recursive: true });
+
 const filteredFiles = files.filter(file => ['.graphql'].some(ext => file.endsWith(ext)));
+
 export default await Promise.all(
   filteredFiles.map(async (file) =>
-    await fs.readFile(`./src/graphql/defTypes/${file}`, 'utf8')
+    await fs.readFile(path.join(__dirname, file), 'utf8')
   )
 )
